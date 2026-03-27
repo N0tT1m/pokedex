d9d3d4e393eeddfe8da169f08b22ca0748bc19d6
Nathan Moritz <nathan.moritz@duocore.dev>
2026-03-26 21:46:23 -0400

    fixed pokedex

--- lib/Widgets/ev_training_finder.dart
+++ lib/Widgets/ev_training_finder.dart
@@ -18,29 +18,29 @@ class _EVTrainingFinderState extends State<EVTrainingFinder> {
   bool isFiltering = false;
   String? errorMessage;
 
-  // Available game versions (matching PokemonDB format)
+  // Available game versions
   final Map<String, String> gameVersions = {
-    'red-blue': 'Red',
+    'red-blue': 'Red/Blue',
     'yellow': 'Yellow',
-    'gold-silver': 'Gold',
+    'gold-silver': 'Gold/Silver',
     'crystal': 'Crystal',
-    'ruby-sapphire': 'Ruby',
+    'ruby-sapphire': 'Ruby/Sapphire',
     'emerald': 'Emerald',
-    'firered-leafgreen': 'Firered',
-    'diamond-pearl': 'Diamond',
+    'firered-leafgreen': 'FireRed/LeafGreen',
+    'diamond-pearl': 'Diamond/Pearl',
     'platinum': 'Platinum',
-    'heartgold-soulsilver': 'Heartgold',
-    'black-white': 'Black',
-    'black-2-white-2': 'Black 2',
-    'x-y': 'X',
-    'omega-ruby-alpha-sapphire': 'Omega Ruby',
-    'sun-moon': 'Sun',
-    'ultra-sun-ultra-moon': 'Ultra Sun',
-    'lets-go-pikachu-lets-go-eevee': 'Lets Go Pikachu',
-    'sword-shield': 'Sword',
-    'brilliant-diamond-and-shining-pearl': 'Brilliant Diamond',
-    'legends-arceus': 'Legends',
-    'scarlet-violet': 'Scarlet',
+    'heartgold-soulsilver': 'HeartGold/SoulSilver',
+    'black-white': 'Black/White',
+    'black-2-white-2': 'Black 2/White 2',
+    'x-y': 'X/Y',
+    'omega-ruby-alpha-sapphire': 'Omega Ruby/Alpha Sapphire',
+    'sun-moon': 'Sun/Moon',
+    'ultra-sun-ultra-moon': 'Ultra Sun/Ultra Moon',
+    'lets-go-pikachu-lets-go-eevee': 'Let\'s Go Pikachu/Eevee',
+    'sword-shield': 'Sword/Shield',
+    'brilliant-diamond-and-shining-pearl': 'Brilliant Diamond/Shining Pearl',
+    'legends-arceus': 'Legends: Arceus',
+    'scarlet-violet': 'Scarlet/Violet',
   };
 
   // EV stat types
